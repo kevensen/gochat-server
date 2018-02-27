6e4d481a2d4984c1aea8f5abc7a08aeb8e41d3b3
Kenneth D. Evensen <kevensen@redhat.com>
2018-02-27 07:12:47 -0500

    Trying to log connections

--- room.go
+++ room.go
@@ -82,7 +82,7 @@ func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		room:     r,
 		userData: objx.MustFromBase64(authCookie.Value),
 	}
-
+	glog.Infoln("Socket created for user: ", client.userData["Name"])
 	r.join <- client
 	defer func() { r.leave <- client }()
 	go client.write()
